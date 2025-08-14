4865e06fe882631858deaffd15e6a1061efb789d
surajgojanur <surajgojanur@gmail.com>
2025-08-14 15:44:54 +0530

    added some inital data sql and set up and dealy with jps defer datasource initiliztion

--- src/main/java/com/spring/EcomProject/controller/ProductController.java
+++ src/main/java/com/spring/EcomProject/controller/ProductController.java
@@ -1,17 +1,29 @@
 package com.spring.EcomProject.controller;
 
 import com.spring.EcomProject.model.Product;
+import com.spring.EcomProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    ProductService productService;
+
     @RequestMapping("/api")
     public String greet(){
         return "Welcome";
+    }
+
+    @GetMapping("/api/products")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
     
 
---
+++ src/main/java/com/spring/EcomProject/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.spring.EcomProject.service;
+import com.spring.EcomProject.model.Product;
+import com.spring.EcomProject.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+}
