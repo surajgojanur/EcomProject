73b57da0b6e0cf0e302b4289f0aabdea39831a4b
surajgojanur <surajgojanur@gmail.com>
2025-08-14 15:02:32 +0530

    Worked on H2

--- src/main/java/com/spring/EcomProject/controller/ProductController.java
+++ src/main/java/com/spring/EcomProject/controller/ProductController.java
@@ -1,4 +1,20 @@
 package com.spring.EcomProject.controller;
 
+import com.spring.EcomProject.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class ProductController {
+
+    @RequestMapping("/api")
+    public String greet(){
+        return "Welcome";
+    }
+    
+
+
+
 }
