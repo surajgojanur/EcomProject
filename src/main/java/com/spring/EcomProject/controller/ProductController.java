79a2142c7a4ce439fc5c8b7f3fc0d347ecdf9aeb
surajgojanur <surajgojanur@gmail.com>
2025-08-14 16:01:40 +0530

    Connect front end with the backend and did apply CrossOrgin o allow asses from aanother portnumber

--- src/main/java/com/spring/EcomProject/controller/ProductController.java
+++ src/main/java/com/spring/EcomProject/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.spring.EcomProject.controller;
 import com.spring.EcomProject.model.Product;
 import com.spring.EcomProject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin
 public class ProductController {
 
     @Autowired
